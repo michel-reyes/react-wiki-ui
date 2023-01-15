a6c25c44c73b79e5f2ce3adeb298e7c42ccdbd4d
Michel <mreyesh85@gmail.com>
2023-01-15 11:31:52 -0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/michel-reyes/react-wiki-ui',
   plugins: [react()],
 })
